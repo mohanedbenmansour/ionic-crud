66952a059dc85e871a808a323ab6f576e8cf2e6e
housseml17 <houssem.toumi@esprit.tn>
2021-06-14 22:19:42 +0100

    final commit

--- server.js
+++ server.js
@@ -1,31 +1,15 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-      return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-var express  = require('express');
-var app      = express();                               
-var morgan = require('morgan');            
-var bodyParser = require('body-parser');    
-var cors = require('cors');
- 
-app.use(morgan('dev'));                                        
-app.use(bodyParser.urlencoded({'extended':'true'}));            
-app.use(bodyParser.json());                                     
-app.use(cors());
-app.use(requireHTTPS);
+const express = require('express');
+const path = require('path');
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/ionic-crud'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/ionic-crud/index.html'));
 });
- 
-app.use(express.static('www'));
-app.set('port', process.env.PORT || 5000);
-app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
